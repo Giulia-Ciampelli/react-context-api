71bdb664f26cb1dbff7293e1831bfe4fb66b6e47
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-27 12:21:33 +0100

    docs: import PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,1 +1,49 @@
-// questo componente conterrà la lista dei post
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+// stile
+import style from "../components/Main.module.css";
+
+export default function PostList({postList, handleTrashPost}) {
+    return (
+        <ul ul >
+        {
+            postList.length ? postList.map((post, index) => <li key={index}>
+                <div className={style.card}>
+                    <h2>
+
+                        {/* prova: aggiungi un modo per accedere allo slug giusto */}
+                        <Link to={`/posts/${post.slug}`}>
+                            {post.title}
+                        </Link>
+                    </h2>
+                    <img src={`http://localhost:3000/${post.image}`} alt={post.title} />
+                    <p>
+                        {post.content}
+                    </p>
+                    <p>
+                        {post.category}
+                    </p>
+                    <div className={style.tags}>
+                        <h2>
+                            Tags:
+                        </h2>
+                        <p>
+                            {Array.isArray(post.tags) && post.tags.length > 0 ? post.tags.join(', ') : 'No tags available'}
+                        </p>
+                    </div>
+                    <p>
+                        {post.public ? 'Post pubblico' : 'Post privato'}
+                    </p>
+                </div>
+
+                {/* RICORDA: inserire sempre type=button per bottoni non per POST, o va in submit per default */}
+                <button type="button" data-slug={post.slug} onClick={() => handleTrashPost(post.slug)}>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+            </li>) : <p>No posts yet</p>
+        }
+        </ul>
+    )
+}
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,8 @@
 // importazioni
 import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { Link } from "react-router-dom";
+
+// componenti
+import PostList from "../components/PostList.jsx";
 
 // modulo stile
 import style from '../components/Main.module.css';
@@ -310,42 +310,7 @@ export default function Posts() {
             {/* #endregion input */}
 
             {/* #region output */}
-            <ul>
-                {postList.length ? postList.map((post, index) => <li key={index}>
-                    <div className={style.card}>
-                        <h2>
-
-                            {/* prova: aggiungi un modo per accedere allo slug giusto */}
-                            <Link to={`/posts/${post.slug}`}>
-                                {post.title}
-                            </Link>
-                        </h2>
-                        <img src={`http://localhost:3000/${post.image}`} alt={post.title} />
-                        <p>
-                            {post.content}
-                        </p>
-                        <p>
-                            {post.category}
-                        </p>
-                        <div className={style.tags}>
-                            <h2>
-                                Tags:
-                            </h2>
-                            <p>
-                                {Array.isArray(post.tags) && post.tags.length > 0 ? post.tags.join(', ') : 'No tags available'}
-                            </p>
-                        </div>
-                        <p>
-                            {post.public ? 'Post pubblico' : 'Post privato'}
-                        </p>
-                    </div>
-
-                    {/* RICORDA: inserire sempre type=button per bottoni non per POST, o va in submit per default */}
-                    <button type="button" data-slug={post.slug} onClick={() => handleTrashPost(post.slug)}>
-                        <FontAwesomeIcon icon={faTrashCan} />
-                    </button>
-                </li>) : <p>No posts yet</p>}
-            </ul>
+            <PostList postList={postList} handleTrashPost={handleTrashPost}/>
             {/* #endregion output */}
         </form>
     )
