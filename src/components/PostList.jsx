97ce5b9cb8f7d4e5e3ca5d1c74de50bea583fa85
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-27 12:33:33 +0100

    docs: add title

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,45 +5,50 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 // stile
 import style from "../components/Main.module.css";
 
-export default function PostList({postList, handleTrashPost}) {
+export default function PostList({ postList, handleTrashPost }) {
     return (
-        <ul>
-        {
-            postList.length ? postList.map((post, index) => <li key={index}>
-                <div className={style.card}>
-                    <h2>
+        <>
+            <h1>
+                I miei post
+            </h1>
+            <ul>
+                {
+                    postList.length ? postList.map((post, index) => <li key={index}>
+                        <div className={style.card}>
+                            <h2>
 
-                        {/* prova: aggiungi un modo per accedere allo slug giusto */}
-                        <Link to={`/posts/${post.slug}`}>
-                            {post.title}
-                        </Link>
-                    </h2>
-                    <img src={`http://localhost:3000/${post.image}`} alt={post.title} />
-                    <p>
-                        {post.content}
-                    </p>
-                    <p>
-                        {post.category}
-                    </p>
-                    <div className={style.tags}>
-                        <h2>
-                            Tags:
-                        </h2>
-                        <p>
-                            {Array.isArray(post.tags) && post.tags.length > 0 ? post.tags.join(', ') : 'No tags available'}
-                        </p>
-                    </div>
-                    <p>
-                        {post.public ? 'Post pubblico' : 'Post privato'}
-                    </p>
-                </div>
+                                {/* prova: aggiungi un modo per accedere allo slug giusto */}
+                                <Link to={`/posts/${post.slug}`}>
+                                    {post.title}
+                                </Link>
+                            </h2>
+                            <img src={`http://localhost:3000/${post.image}`} alt={post.title} />
+                            <p>
+                                {post.content}
+                            </p>
+                            <p>
+                                {post.category}
+                            </p>
+                            <div className={style.tags}>
+                                <h2>
+                                    Tags:
+                                </h2>
+                                <p>
+                                    {Array.isArray(post.tags) && post.tags.length > 0 ? post.tags.join(', ') : 'No tags available'}
+                                </p>
+                            </div>
+                            <p>
+                                {post.public ? 'Post pubblico' : 'Post privato'}
+                            </p>
+                        </div>
 
-                {/* RICORDA: inserire sempre type=button per bottoni non per POST, o va in submit per default */}
-                <button type="button" data-slug={post.slug} onClick={() => handleTrashPost(post.slug)}>
-                    <FontAwesomeIcon icon={faTrashCan} />
-                </button>
-            </li>) : <p>No posts yet</p>
-        }
-        </ul>
+                        {/* RICORDA: inserire sempre type=button per bottoni non per POST, o va in submit per default */}
+                        <button type="button" data-slug={post.slug} onClick={() => handleTrashPost(post.slug)}>
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </li>) : <p>No posts yet</p>
+                }
+            </ul>
+        </>
     )
 }
